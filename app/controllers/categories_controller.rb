13fa6b202422f31465cb3f9640c632188e905a43
kchn9 <mat.kuchnia@gmail.com>
2022-09-10 19:35:47 +0200

    Remove searchbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,15 @@
 class CategoriesController < ApplicationController
+  before_action :set_category
+
   def show
+    @courses = Course.all
+    @categories = Category.all
+    @should_render_categories = true
+  end
+
+  private
+
+  def set_category
+    @category = Category.find_by(id: params[:id])
   end
 end
