96d5a14c70f88f2734fc0313f7b87ec12814f280
kchn9 <mat.kuchnia@gmail.com>
2022-09-12 16:29:30 +0200

    Prepare layout

---
+++ app/controllers/learning_controller.rb
@@ -0,0 +1,7 @@
+class LearningController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @courses = Course.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
   get '/categories/:id', to: 'categories#show', as: 'categories'
+  get '/learning/', to: 'learning#index', as: 'learning'
   root 'courses#index'
 end
