98dfe18e165cb67a79c982d4ec17fef3bc6267ae
kchn9 <mat.kuchnia@gmail.com>
2022-09-12 17:48:40 +0200

    Self-enroll validation

--- app/models/enrolled_course.rb
+++ app/models/enrolled_course.rb
@@ -1,6 +1,13 @@
 class EnrolledCourse < ApplicationRecord
   validates :course_id, uniqueness: { scope: [:user_id] }
+  validate :user_not_enroll_own_course
 
   belongs_to :user
   belongs_to :course
+
+  def user_not_enroll_own_course
+    if Course.find(course_id).author_id == user_id
+      errors.add(:user_id, "can't enroll self course")
+    end
+  end
 end
